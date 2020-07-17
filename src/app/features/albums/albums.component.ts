65ea6da3a2e5090ae4ad1dd5d132b683c729f9d0
Mohtadi Bakali <teddybak@hotmail.com>
2020-07-17 18:01:02 +0100

    completado

--- src/app/features/albums/albums.component.ts
+++ src/app/features/albums/albums.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchItem } from 'src/app/models/searchItem.model';
+import { ActivatedRoute } from '@angular/router';
+import { ItunesService } from 'src/app/services/itunes.service';
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
-  styleUrls: ['./albums.component.css']
+  styleUrls: ['./albums.component.css'],
 })
 export class AlbumsComponent implements OnInit {
+  data: SearchItem[] = [];
+  artist: any;
 
-  constructor() { }
+  objeto: Object = {
+    search: null,
+    entity: 'album',
+  };
+
+  constructor(private route: ActivatedRoute, private itunes: ItunesService) {}
 
   ngOnInit(): void {
-  }
+    this.route.queryParams.subscribe((params) => {
+      this.objeto['search'] = params['search'];
+    });
 
+    this.itunes.getArtistCollection(this.objeto).subscribe((datos) => {
+      this.data = datos;
+    });
+  }
 }
